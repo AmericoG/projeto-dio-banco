0e3a7c690d35455b6e5394cdb100ad05d11d0d16
AmericoG <gabriel.americodc@outlook.com>
2022-05-19 02:38:18 -0300

    first commit

---
+++ src/Conta.java
@@ -0,0 +1,52 @@
+public abstract class Conta {
+
+    private static final int AGENCIA_PADRAO = 1;
+    private static int sequencial = 1;
+
+    private int agencia;
+    private int numero;
+    private double saldo;
+    private Cliente cliente;
+
+    public Conta(Cliente cliente) {
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = sequencial++;
+        this.cliente = cliente;
+    }
+
+    public void sacar(double valor) {
+        this.saldo -= valor;
+    }
+
+    public void depositar(double valor) {
+        this.saldo += valor;
+    }
+
+    public void transferir(double valor, Conta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
+
+    protected void imprimirInfos(){
+        System.out.println("Titular: " + this.cliente.getNome());
+        System.out.println("Agência: " + this.agencia);
+        System.out.println("Número: " + this.numero);
+        System.out.println("Saldo: " + this.saldo);
+    }
+
+    public static int getSequencial() {
+        return sequencial;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
